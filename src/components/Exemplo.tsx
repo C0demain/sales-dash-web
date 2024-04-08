6915c771189d8553c0adf3852fba319ad8f66718
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2024-04-08 10:28:52 -0300

    Cria estrutura básica do front

---
+++ src/components/Exemplo.tsx
@@ -0,0 +1,14 @@
+import { Component, ReactNode } from "react";
+import './Exemplo.css'
+
+class Exemplo extends Component{
+    render(): ReactNode {
+        return (
+            <div className="exemplo">
+                <h1>Esta é uma página de exemplo</h1>
+            </div>
+        )
+    }
+}
+
+export default Exemplo
