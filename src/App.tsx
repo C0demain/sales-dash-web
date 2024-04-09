2313d02ebb5070a61e577ebed3b02d1b6886cb1f
Abner <abner.rmachado@gmail.com>
2024-04-09 09:37:29 -0300

    arruma frontend login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Exemplo from './components/Exemplo'
-import './App.css'
+import styled from "styled-components";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./components/login";
+import  Home  from "./components/home";
+import { RequireAuth } from "react-auth-kit";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 function App() {
-
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/exemplo' element={<Exemplo/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    <AppContainer>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth loginPath="/login">
+              <Home />
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </AppContainer>
+  );
 }
 
-export default App
+export default App;
