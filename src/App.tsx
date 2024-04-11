149d11bff0efbe3335bb00a0368d9f2f5fdfa4b0
Enzopereira01 <126246486+Enzopereira01@users.noreply.github.com>
2024-04-11 09:07:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import  Home  from "./components/home";
 import { RequireAuth } from "react-auth-kit";
 import CadastroUser from "./components/CadastroUser/pages/CadastroUser";
 import SystemComission from "./components/SystemComission/page/SystemComission";
+import CadastroVendas from "./components/CadastroVenda/pages/CadastroVendas";
 
 
 const AppContainer = styled.div`
@@ -27,9 +28,11 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path='/cadastro' element={<CadastroUser/>}></Route>
         <Route path='/comission' element={<SystemComission/>}></Route>
+        <Route path='/cadastroVendas' element={<CadastroVendas/>}></Route>
       </Routes>
     </AppContainer>
   );
 }
 
 export default App;
+
