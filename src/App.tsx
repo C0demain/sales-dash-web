c15270480933dc5745cff87ed940a5d1aad53cb4
Abner <abner.rmachado@gmail.com>
2024-04-19 20:10:21 -0300

    autenticação de acesso as rotas e token no cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 import { Route, Routes } from "react-router-dom";
-import { Login } from "./components/Login";
 import CadastroUser from "./components/CadastroUser/pages/CadastroUser";
 import CadastroProduto from "./components/CadastroProduto/pages/CadastroProduto";
 import CadastroCliente from "./components/CadastroCliente/pages/CadastroCliente";
 import CommissionRegister from "./components/SystemComission/page/CommissionRegister";
-// import CadastroVendas from "./components/CadastroVenda/pages/CadastroVendas";
 import UploadExcelPage from "./components/CarregadorArquivo/pages/Carregador";
-
 import { Dashboard } from "./components/dashboard/dashboard";
 import { CadastroVenda } from "./components/cadastroVendas/pages/cadastroVenda";
 import CommissionList from "./components/SystemComission/page/CommissionList";
 import ListaCliente from "./components/ExibeCliente/pages/ListaCliente";
+import { Login } from "./components/login";
+import { RequireAuth } from "react-auth-kit";
 
 const AppContainer = styled.div`
   width: 100%;
@@ -22,27 +21,101 @@ function App() {
   return (
     <AppContainer>
       <Routes>
+        {/* Rota de Login sem autenticação*/}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Rota de Dashboard */}
         <Route
           path="/dashboard"
           element={
-            <Dashboard/>
+            <RequireAuth loginPath="/login">
+              <Dashboard />
+            </RequireAuth>
           }
-        ></Route>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path='/sellers/register' element={<CadastroUser/>}></Route>
-        <Route path='/commissions/register' element={<CommissionRegister/>}></Route>
-        <Route path='/commissions' element={<CommissionList/>}></Route>
-        <Route path='/commissions/register' element={<CommissionRegister/>}></Route>
-        <Route path ='/sells/table' element= {<UploadExcelPage/>}></Route>
-        <Route path="/sells/register" element={<CadastroVenda/>}></Route>
-        <Route path='/product/register' element={<CadastroProduto/>}></Route>
-        <Route path='/client/register' element={<CadastroCliente/>}></Route>
-        <Route path='/client/list' element={<ListaCliente/>}></Route>
+        />
+
+        {/* Rota de Cadastro de Vendedores */}
+        <Route
+          path="/sellers/register"
+          element={
+            <RequireAuth loginPath="/login">
+              <CadastroUser />
+            </RequireAuth>
+          }
+        />
+
+        {/* Rota de Cadastro de Comissões */}
+        <Route
+          path="/commissions/register"
+          element={
+            <RequireAuth loginPath="/login">
+              <CommissionRegister />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/commissions"
+          element={
+            <RequireAuth loginPath="/login">
+              <CommissionList />
+            </RequireAuth>
+          }
+        />
+
+        {/* Rota de Tabela de Vendas */}
+        <Route
+          path="/sells/table"
+          element={
+            <RequireAuth loginPath="/login">
+              <UploadExcelPage />
+            </RequireAuth>
+          }
+        />
+
+        {/* Rota de Cadastro de Vendas */}
+        <Route
+          path="/sells/register"
+          element={
+            <RequireAuth loginPath="/login">
+              <CadastroVenda />
+            </RequireAuth>
+          }
+        />
+
+        {/* Rota de Cadastro de Produtos */}
+        <Route
+          path="/product/register"
+          element={
+            <RequireAuth loginPath="/login">
+              <CadastroProduto />
+            </RequireAuth>
+          }
+        />
+
+        {/* Rota de Cadastro de Clientes */}
+        <Route
+          path="/client/register"
+          element={
+            <RequireAuth loginPath="/login">
+              <CadastroCliente />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/client/list"
+          element={
+            <RequireAuth loginPath="/login">
+              <ListaCliente />
+            </RequireAuth>
+          }
+        />
+
       </Routes>
     </AppContainer>
   );
 }
 
 export default App;
-
