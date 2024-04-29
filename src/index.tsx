45feb9cb7e34befa4095c2c28791e1f046c33aae
Abner <abner.rmachado@gmail.com>
2024-04-29 07:53:04 -0300

    arruma warn react 18 no index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import { AppRoutes } from 'routes/AppRoutes';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { AppRoutes } from "routes/AppRoutes";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <AppRoutes />
-  </React.StrictMode>,
-  document.getElementById('root') 
+  </React.StrictMode>
 );
