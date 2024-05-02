ac490252791cc34e0a4c81235ca3242e7f4753e3
Enzopereira01 <126246486+Enzopereira01@users.noreply.github.com>
2024-05-02 16:40:32 -0300

    Update Navbar Adm.
    Criação de um botão único que quando clicado aparece os botão de cadastro que já possuíam na navbar.

--- src/components/AdminNavbar/AdminNavbar.tsx
+++ src/components/AdminNavbar/AdminNavbar.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.css'
 
 const Navbargest = () => {
     const navigate = useNavigate()
+    const [showCadastro, setShowCadastro] = useState(false);
   
     const handleLogout = () => {
       localStorage.removeItem('user');
@@ -15,10 +17,15 @@ const Navbargest = () => {
         <ul>
           <li><button onClick={e => navigate('/dashboard')} className='Botao_barra'>Dashboard</button></li>
           <li><button onClick={e => navigate('/sells/table')}className='Botao_barra'>Upload Planilha</button></li>
-          <li><button onClick={e => navigate('/sells/register')} className='Botao_barra'>Cadastro de Vendas</button></li>
-          <li><button onClick={e => navigate('/sellers/register')}className='Botao_barra'>Cadastro de Usuários</button></li>
-          <li><button onClick={e => navigate('/product/register')}className='Botao_barra'>Cadastro de Produtos</button></li>
-          <li><button onClick={e => navigate('/client/register')}className='Botao_barra'>Cadastro de Clientes</button></li>
+          <li><button onClick={e => setShowCadastro(!showCadastro)} className='Botao_barra'>Cadastro</button></li>
+                {showCadastro && (
+                    <>
+                        <li><button onClick={e => navigate('/sells/register')} className='Botao_barra'>Cadastro de Vendas</button></li>
+                        <li><button onClick={e => navigate('/sellers/register')} className='Botao_barra'>Cadastro de Usuários</button></li>
+                        <li><button onClick={e => navigate('/product/register')} className='Botao_barra'>Cadastro de Produtos</button></li>
+                        <li><button onClick={e => navigate('/client/register')} className='Botao_barra'>Cadastro de Clientes</button></li>
+                    </>
+                )}
           <li><button onClick={e => navigate('/client/list')}className='Botao_barra'>Exibe Clientes</button></li>
           <li><button onClick={e => navigate('/product/list')}className='Botao_barra'>Exibe Produtos</button></li>
           <li><button onClick={e => navigate('/sell/showsales')}className='Botao_barra'>Exibe Vendas</button></li>
