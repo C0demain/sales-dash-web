bfa07cdc50d96fc90fb660eb504f75602389b69c
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2024-06-11 08:19:44 -0300

    Corrige gráfico de produtos do gestor

--- src/components/ProductChart/ProductChart.tsx
+++ src/components/ProductChart/ProductChart.tsx
@@ -1,36 +1,31 @@
 import { useCallback, useEffect, useState } from 'react';
 import { formatDateToBack } from 'util/formatters';
 import { Chart } from "react-google-charts";
-import './index.css';
-import { Switch, Select, Spin, Empty } from 'antd';
+import Switch from '@mui/material/Switch';
+import { Empty, Select } from 'antd';
 import { apiInstance } from 'services/api';
+import { useAuth } from 'context/AuthProvider/useAuth';
+import SelectProduct from 'components/SelectProduct/SelectProduct';
 
-interface Product {
-  id: number;
-  name: string;
-}
-
-export default function BasicLineChart() {
-  const [dataSells, setDataSells] = useState<any[]>([]);
-  const [data, setData] = useState<any[]>([["Mês", "Valor vendido"]]);
-  const [startDate, setStartDate] = useState<any>();
-  const [endDate, setEndDate] = useState<any>();
-  const [selectedProducts, setSelectedProducts] = useState<string[]>([]);
-  const [title, setTitle] = useState<any>('Valor vendido');
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState<Product[]>([]);
-  const [groupedProducts, setGroupedProducts] = useState<Map<string, Product[]>>(new Map());
-  const [monthDiff, setMonthDiff] = useState<number>(11); 
-  const customIndicator = <div style={{ display: 'none' }} />;
-  const today = new Date();
+export default function LineChartSeller() {
+  const [dataSells, setDataSells] = useState<any[]>([])
+  const [data, setData] = useState<any[]>([["Mês", "Valor vendido"]])
+  const [startDate, setStartDate] = useState<any>()
+  const [endDate, setEndDate] = useState<any>()
+  const [product, setProduct] = useState<number>()
+  const [checked, setChecked] = useState(true)
+  const [title, setTitle] = useState<any>('Valor vendido')
+  const [monthDiff, setMonthDiff] = useState<any>(5)
+  const today = new Date()
 
   const periodOptions = [
     { label: 'Últimos 12 meses', value: 11 },
     { label: 'Últimos 6 meses', value: 5 },
     { label: 'Últimos 3 meses', value: 2 },
+    { label: 'Mês atual', value: 0 }
   ];
 
-  const [options, setOptions] = useState<any>({
+  const [options] = useState<any>({
     colors: ["#8e0152", "#276419"],
     pointSize: 10,
     animation: {
@@ -38,141 +33,109 @@ export default function BasicLineChart() {
       easing: "linear",
       startup: true,
     },
-    seriesType: 'bar',
+    seriesType: 'bars',
     series: [
       { type: 'bars', color: '#1976d2' },
       { type: 'line', color: '#001529' },
     ],
-    legend: { position: "bottom" },
+    legend: { position: "none" },
+    chart: {
+      title: `Vendas por mês`,
+    },
   });
-
-  const fetchProducts = useCallback(async () => {
-    const response = await apiInstance.get("http://localhost:8000/api/v1/products/getAll");
-    setProducts(response.data.products);
-  }, []);
-
-    const grouped = productsData.reduce((acc: Map<string, Product[]>, product: Product) => {
-      if (!acc.has(product.name)) {
-       acc.set(product.name, []);
-     }
-      acc.get(product.name)?.push(product);
-       return acc;
-     }, new Map());
-
-    setProducts(productsData);
-    setGroupedProducts(grouped);
-   }, []);
 
   const setDates = useCallback(() => {
     const year = today.getFullYear();
     const month = today.getMonth();
     const targetMonth = month - monthDiff;
     let targetYear = year;
+
     if (targetMonth < 0) {
       targetYear -= 1;
     }
+
     const adjustedMonth = (targetMonth + 12) % 12;
-    const startDate = new Date(targetYear, adjustedMonth, 1);
+    let startDate = new Date(targetYear, adjustedMonth, 1);
 
     setStartDate(formatDateToBack(startDate));
     setEndDate(formatDateToBack(today));
-  }, [today, monthDiff]);
+  }, [monthDiff, today]);
 
   const getSellsPeriod = useCallback(async () => {
-    setLoading(true);
     setDates();
+    let url = "http://localhost:8000/api/v1/dashboard/date/product";
 
-    let url = "http://localhost:8000/api/v1/dashboard/date";
-    const startDateFilter = startDate ? 'startDate=' + startDate : "";
-    const endDateFilter = endDate ? 'endDate=' + endDate : "";
-    const productsFilter = selectedProducts.length > 0 ? 'products=' + selectedProducts.join(',') : "";
+    try {
+      const response = await apiInstance.get(url, {
+        withCredentials: false, params:{
+          startDate,
+          endDate,
+          productId: product
+        }
+      });
+      console.log(response.data.stats)
+      setDataSells(response.data.stats);
+    } catch (error) {
+      console.error('Erro ao buscar os dados de vendas:', error);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startDate, endDate, product, monthDiff]);
 
-    let queryParams = [startDateFilter, endDateFilter, productsFilter];
-    const query = queryParams.filter(e => e !== '').join('&');
-    url += query !== "" ? "?" + query : "";
-
-    const response = await apiInstance.get(url, { withCredentials: false });
-    setDataSells(response.data.stats);
-    setLoading(false);
-  }, [startDate, endDate, selectedProducts, setDates]);
+  const setDataStats = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (data[0][1] === "Valor vendido") {
+      let chartData: Array<any> = [["Mês", "Comissão de venda", "Comissão de venda"]];
+      dataSells.forEach(stat => {
+        chartData.push([stat.month, stat.totalCommissionValue, stat.totalCommissionValue]);
+      });
+      setData(chartData);
+      setTitle('Comissão de venda mensal');
+    } else {
+      let chartData: Array<any> = [["Mês", "Valor vendido", "Valor vendido"]];
+      dataSells.forEach(stat => {
+        chartData.push([stat.month, stat.totalValue, stat.totalValue]);
+      });
+      setData(chartData);
+      setTitle('Valor vendido mensalmente');
+    }
+    setChecked(event.target.checked);
+  }
 
   useEffect(() => {
-    fetchProducts();
     getSellsPeriod();
-  }, [fetchProducts, getSellsPeriod]);
+  }, [getSellsPeriod]);
 
   useEffect(() => {
     if (dataSells.length > 0) {
-      let chartData: Array<any> = [["Mês", ...selectedProducts.map(prod => prod)]];
-      const dataMap = new Map();
-
+      let chartData: Array<any> = [["Mês", "Valor vendido", "Valor vendido"]];
       dataSells.forEach(stat => {
-        if (!dataMap.has(stat.month)) {
-          dataMap.set(stat.month, {});
-        }
-        const monthData = dataMap.get(stat.month);
-        monthData[stat.productName] = stat.totalValue;
-        dataMap.set(stat.month, monthData);
+        chartData.push([stat.month, stat.totalValue, stat.totalValue]);
       });
-
-      dataMap.forEach((value, key) => {
-        const row = [key, ...selectedProducts.map(prod => value[prod] || 0)];
-        chartData.push(row);
-      });
-
       setData(chartData);
     }
-  }, [dataSells, selectedProducts]);
-
-  const originalWarn = console.warn;
-
-  console.warn = function (...args) {
-    const arg = args && args[0];
-    if (arg && arg.includes('Attempting to load version \'51\' of Google Charts')) return;
-    originalWarn(...args);
-  };
+  }, [dataSells]);
 
   return (
     <div>
       <div className='titleChart'>
-        <Spin spinning={loading} indicator={customIndicator}>
-          {dataSells.length > 0 ? (
-            <>
-              <div style={{ display: 'flex-wrap', alignItems: 'center', justifyContent: 'flex-start', marginBottom: '3vh', marginTop: '3vh', minWidth: '100%' }}>
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                  <Select
-                    mode="multiple"
-                    showSearch
-                    placeholder="Selecione produtos"
-                    onChange={(values) => setSelectedProducts(values)}
-                    style={{ width: 200 }}
-                    options={products.map((product) => ({
-                      label: product.name,
-                      value: product.name,
-                    }))}
-                  />
-                  <Select
-                    style={{ marginLeft: '10px' }}
-                    options={periodOptions}
-                    onSelect={(value) => setMonthDiff(value)}
-                    defaultValue={11}
-                  />
-                </div>
-                <h3>{title}</h3>
-              </div>
-              <Chart
-                chartType="ComboChart"
-                data={data}
-                options={options}
-                width="75vh"
-                height="35vh"
-              />
-            </>
-          ) : (
-            !loading && <Empty description="Não há dados registrados." />
-          )}
-        </Spin>
+        <Switch checked={checked} onChange={setDataStats} />
+        <h3>{title}</h3>
+        <Select
+          options={periodOptions}
+          onSelect={(value) => setMonthDiff(value)}
+          defaultValue={5}
+        />
+        <SelectProduct controlState={[product, setProduct]} dataKey='id'/>
       </div>
+      {dataSells.length > 0 ?
+        <Chart
+          chartType="ComboChart"
+          data={data}
+          options={options}
+          width="80vh"
+          height="35vh"
+          loader={<div>Carregando Gráfico</div>}
+        /> : <Empty description="Você não possui nenhum venda nesse período" />
+      }
     </div>
-  );
+  )
 }
